07210202be1811c398d3af0b4e14d6cb0424357c
avavion <avavionmvm@gmail.com>
2021-10-31 16:44:42 +0300

    added filter

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 const PATH = location.pathname;
-const PASSWORD = localStorage.getItem("code") === null ? "0000" : localStorage.getItem('code');
+const PASSWORD = localStorage.getItem("code") === null ? "0000" : JSON.parse(localStorage.getItem('code'));
 
 if (localStorage.getItem('isAuth') === null) {
     localStorage.setItem('isAuth', JSON.stringify(false));
@@ -26,40 +26,73 @@ if (PATH.match('wallet.html')) {
     if (JSON.parse(localStorage.getItem("isAuth")) === false) {
         location.href = location.origin + '/wsr-wallet/index.html';
     }
+    //
+    // const wallet = {
+    //     history: [
+    //         {
+    //             type: {
+    //                 display: "Доход",
+    //                 value: "income"
+    //             },
+    //             name: "Заробатная плата",
+    //             amount: 30000,
+    //             date: Date.now()
+    //         },
+    //         {
+    //             type: {
+    //                 display: "Расход",
+    //                 value: "expenses"
+    //             },
+    //             name: "Налоги",
+    //             amount: (30000 * 0.13),
+    //             date: new Date().setDate(new Date().getDate() - 14)
+    //         },
+    //         {
+    //             type: {
+    //                 display: "Доход",
+    //                 value: "income"
+    //             },
+    //             name: "Заробатная плата",
+    //             amount: 30000,
+    //             date: new Date().setDate(new Date().getDate() - 3)
+    //         },
+    //         {
+    //             type: {
+    //                 display: "Расход",
+    //                 value: "expenses"
+    //             },
+    //             name: "Налоги",
+    //             amount: (30000 * 0.13),
+    //             date: new Date().setDate(new Date().getDate() - 22)
+    //         },
+    //         {
+    //             type: {
+    //                 display: "Доход",
+    //                 value: "income"
+    //             },
+    //             name: "Заробатная плата",
+    //             amount: 30000,
+    //             date: new Date().setDate(new Date().getDate() - 5)
+    //         },
+    //         {
+    //             type: {
+    //                 display: "Расход",
+    //                 value: "expenses"
+    //             },
+    //             name: "Налоги",
+    //             amount: (30000 * 0.13),
+    //             date: new Date().setMonth(new Date().getMonth() - 2)
+    //         }
+    //     ],
+    //     categories: [
+    //         {display: "Еда", value: 'food'},
+    //         {display: "Автомобиль", value: 'car'},
+    //         {display: "Продукты", value: 'products'},
+    //         {display: "Топливо", value: 'fuel'}
+    //     ]
+    // }
 
-    const wallet = {
-        history: [
-            {
-                type: {
-                    display: "Доход",
-                    value: "income"
-                },
-                name: "Заробатная плата",
-                amount: 30000,
-                timestamp: Date.now()
-            },
-            {
-                type: {
-                    display: "Расход",
-                    value: "expenses"
-                },
-                name: "Налоги",
-                amount: (30000 * 0.13),
-                timestamp: Date.now()
-            }
-        ],
-        categories: [
-            {display: "Еда", value: 'food'},
-            {display: "Автомобиль", value: 'car'},
-            {display: "Продукты", value: 'products'},
-            {display: "Топливо", value: 'fuel'}
-        ]
-    }
-
-    // const wallet = getWallet();
-    const income = getIncome();
-
-    console.log(getBalance());
+    const wallet = getWallet();
 
     function getWallet() {
         if (localStorage.getItem("wallet") === null) {
@@ -123,12 +156,33 @@ if (PATH.match('wallet.html')) {
         return wallet.history;
     }
 
-    function changePassword() {
+    function changePassword(password) {
+        if (password.length >= 4) {
+            localStorage.setItem('code', JSON.stringify(password));
+            return true;
+        }
 
+        return false;
     }
 
     function resetPassword() {
+        localStorage.removeItem('code');
+        return true;
+    }
 
+    function sortByDay(dataset) {
+        const day = new Date().setDate(new Date().getDate() - 1);
+        return dataset.filter((data) => data.date >= day)
+    }
+
+    function sortByWeek(dataset) {
+        const week = new Date().setDate(new Date().getDate() - 7);
+        return dataset.filter((data) => data.date >= week)
+    }
+
+    function sortByMonth(dataset) {
+        const month = new Date().setMonth(new Date().getMonth() - 1);
+        return dataset.filter((data) => data.date >= month);
     }
 
     function logout() {
@@ -139,14 +193,15 @@ if (PATH.match('wallet.html')) {
     }
 
 
-    function displayHistory() {
-        const dataset = getHistory();
+    function displayHistory(dataset) {
         const el = document.getElementById('stats');
         const list = el.querySelector('ul.list');
 
+        list.innerHTML = '';
+
         dataset.map((data, index) => {
             list.insertAdjacentHTML(`beforeend`, `
-                <li class="list-item ${data.type.value}">[${data.type.display}] ${data.name} - ${data.amount} рублей. (${new Date(data.timestamp).toLocaleDateString()})</li>
+                <li class="list-item ${data.type.value}">[${data.type.display}] ${data.name} - ${data.amount} рублей. (${new Date(data.date).toLocaleDateString()})</li>
             `);
         });
     }
@@ -161,8 +216,55 @@ if (PATH.match('wallet.html')) {
         }
     }
 
-    displayHistory();
+    // displayHistory(wallet.history);
+
+    displayHistory(sortByDay(wallet.history));
 
     document.querySelector('.button--logout').addEventListener('click', logout);
     displaySummary();
+
+    const settings = document.getElementById('settings');
+    const passwordField = settings.querySelector('.form__input');
+    const passwordButtons = settings.querySelectorAll('button');
+
+    console.log(passwordField);
+
+    for (const button of passwordButtons) {
+        if (button.classList.contains('button--danger')) {
+            button.addEventListener('click', resetPassword);
+        } else {
+            button.addEventListener('click', () => changePassword(passwordField.value.trim()));
+        }
+    }
+
+    const logoutButton = settings.querySelector(".button.button--logout");
+    logoutButton.addEventListener("click", logout);
+
+    const filter = document.querySelector(".filter");
+
+    filter.addEventListener("click", (e) => {
+        const type = e.target.getAttribute('data-type');
+
+        switch (type) {
+            case "day":
+                displayHistory(sortByDay(wallet.history));
+                break;
+
+            case "week":
+                displayHistory(sortByWeek(wallet.history));
+                break;
+
+            case "month":
+                displayHistory(sortByMonth(wallet.history));
+                break;
+
+            case "clear":
+                displayHistory(wallet.history);
+                break;
+
+            default:
+                return;
+                break;
+        }
+    })
 }
